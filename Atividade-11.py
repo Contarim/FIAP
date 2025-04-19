5200ff00cd3d7cffd21f1871471868b6c9024f58
João Contarim <joaocontar@hotmail.com>
2025-04-18 23:03:37 -0300

    Treino com listas e exibição delas

---
+++ Atividade-11.py
@@ -0,0 +1,22 @@
+#criando a lista vazia
+bebida = []
+
+#criando uma lista com elementos
+bebida = ['Guaraná', 'Cerveja', 'Leite', 'Suco' ]
+
+#exibe a lista toda
+print(bebida)
+
+#exibe apenas 1 elemento
+print(bebida[3])
+
+#exibe 1 elemento de trás para a frente
+print(bebida[-2])
+
+#exibe mais de um elemento
+print(bebida[0:4])
+
+#Exibe a lista inteira sem os colchetes e vírgula utilizando FOR
+for nome in bebida:
+    print(nome)
+
