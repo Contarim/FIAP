9bf7f6ed4086dce3a0d702813f8535928827c8ba
João Contarim <joaocontar@hotmail.com>
2025-04-19 15:07:30 -0300

    Calcula valores chamando função

---
+++ Atividade-18.py
@@ -0,0 +1,11 @@
+#import calculo
+#from calculo import soma
+#from calculo import subtracao
+#from calculo import divisao
+#from calculo import multiplicacao
+from calculo import soma, subtracao, divisao, multiplicacao
+
+print(f'Retorno da função de soma: {soma(5, 4)}')
+print(f'Retorno da função de subtração: {subtracao(5, 4)}')
+print(f'Retorno da função de divisão: {divisao(5, 4)}')
+print(f'Retorno da função de multiplicação: {multiplicacao(5, 4)}')
