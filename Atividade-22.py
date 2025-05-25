db5e715087a3e49e32aeb2f9ac68ab6bafeb9499
João Contarim <joaocontar@hotmail.com>
2025-05-25 11:10:36 -0300

    Release: Aprendendo a criar dicionários, mostrando chaves, valores e os items dentro dele

---
+++ Atividade-22.py
@@ -0,0 +1,37 @@
+#Aprendendoo a criar e mostrar chaves e valores de um dicionário
+dicionario = {
+    "Produto 1": "Notebook",
+    "Produto 2": "Geladeira",
+    "Produto 3": "Microondas",
+    "Produto 4": "Cama",
+    "Produto 5": "Ventilador",
+    "Produto 6": "Mesa",
+}
+print("\n")
+print(dicionario["Produto 2"])
+
+print("\n")
+print(dicionario.get("Produto 3"))
+
+print("\n")
+print(dicionario.keys())
+
+print("\n")
+for chave in dicionario.keys():
+    print(dicionario[chave])
+
+print("\n")    
+print(len(dicionario.values()))
+
+print("\n")
+print("Produto 1" in dicionario)
+
+print("\n")
+print(dicionario.items())
+
+print("\n")
+for item in dicionario.items():
+    produto, descricao = item
+    print(f"O {produto} é um(a) {descricao}")
+
+
